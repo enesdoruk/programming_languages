512bf346eaeafec27e2d83b08d76b5139d8524f1
Abdullah Enes DORUK <abdullahenesdoruk@gmail.com>
2021-11-18 15:43:44 +0300

    Add files via upload

---
+++ C-Fundamentals/array4/array4/array4.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <array>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main(){
+	const size_t arraySize = 7;
+	array < string, arraySize> colors = { "indigo", "blue", "orange", "purple", "yellow",
+											"black", "brown" };
+
+	cout << "unsorted" << endl;
+	for (string color : colors)
+		cout << "color=  " << color << endl;
+
+	sort( colors.begin(), colors.end() );
+
+	cout << "after sorted    " << endl;
+	for (string item : colors)
+		cout << "color    " << item << endl;
+
+	bool found = binary_search( colors.begin(), colors.end(), "indigo" );
+	cout << "found indigo    " << found << endl;
+
+	found = binary_search(colors.begin(), colors.end(), "blue" );
+	cout << "found blue   " << found << endl;
+}
+
