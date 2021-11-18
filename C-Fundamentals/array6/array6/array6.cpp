4f38132f2ce7420c0bfabb9e2697c11d96b1c13e
Abdullah Enes DORUK <abdullahenesdoruk@gmail.com>
2021-11-18 16:09:54 +0300

    Add files via upload

---
+++ C-Fundamentals/array6/array6/array6.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include < array> 
+#include "helper.h"
+
+using namespace std;
+
+int main(){
+
+	array < array < int, GradeBook::tests >, GradeBook::students > grades =
+	{ 87,96,70,
+	  68,23,43,
+	  43,45,75,
+	  23,75,23,
+	  15,65,43,
+	  87,42,43,
+	  34,76,44,
+	  87,65,87,
+	  14,65,45,
+	  95,64,75 };
+
+	GradeBook myGradeBook(
+		"CS PROGRAMMING", grades);
+	myGradeBook.displayMessage();
+	myGradeBook.processGrades();
+}
+
