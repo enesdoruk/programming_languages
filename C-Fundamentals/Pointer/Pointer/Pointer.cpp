54778c5122abc8e69fe38fd4d1bcb594572d9047
Abdullah Enes DORUK <abdullahenesdoruk@gmail.com>
2021-11-18 16:11:29 +0300

    Add files via upload

---
+++ C-Fundamentals/Pointer/Pointer/Pointer.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int a = 7;
+	int *aPtr = &a;
+	cout << "a adress " << &a << " aptr value " << aPtr << " a value" << a << "*aPtr value " << *aPtr;
+}
